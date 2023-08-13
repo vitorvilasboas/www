ead47c13d162a4c7c8a06bb540fa36f09b4c8989
vitorvilasboas <vitormendesvilasboas@gmail.com>
2023-08-13 00:55:53 -0300

    ..

--- js/demo_frontend/script.js
+++ js/demo_frontend/script.js
@@ -4,6 +4,6 @@ let list = document.getElementById("vitor")
 
 function showHide(e){
 	console.log(e)
-if(show){show = false; list.style.display = 'none'; e.innerHTML = "show";}
-else{show = true; list.style.display = 'block' ; e.innerHTML = "hide"}
+    if(show){show = false; list.style.display = 'none'; e.innerHTML = "show";}
+    else{show = true; list.style.display = 'block' ; e.innerHTML = "hide"}
 }
